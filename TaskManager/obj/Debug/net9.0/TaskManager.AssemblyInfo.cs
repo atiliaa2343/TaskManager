87abe881998fee974b560b8f7959f266a5688572
atiliaa2343 <atiliaa.thomas@gmail.com>
2025-10-02 14:45:17 -0400

    added docker file and compose

--- TaskManager/obj/Debug/net9.0/TaskManager.AssemblyInfo.cs
+++ TaskManager/obj/Debug/net9.0/TaskManager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27e017331f9b8562c89c6162f27b2425bf3e1d23")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
